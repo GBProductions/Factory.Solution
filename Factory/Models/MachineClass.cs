688a2fe3766a5a31cc0e12cb303dc3aa4237e50a
Garrett Brown <garrettpaulbrown@gmail.com>
2021-06-05 11:52:12 -0700

    Functioning program for many to one relationship.

---
+++ Factory/Models/MachineClass.cs
@@ -0,0 +1,12 @@
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public int EngineerId { get; set;}
+    public virtual Engineer Engineer { get; set; }
+  }
+}
